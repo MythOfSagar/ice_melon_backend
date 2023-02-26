0beeabc536c8b663c5563ccb52d80a12dbcddb48
Sagar1079 <sagarwankhade896@gmail.com>
2023-02-26 14:50:36 +0530

    Cors Error manage

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors=require('cors')
 
 const app = express();
 
-app.use(cors({origin:'*'}))
+//app.use(cors({origin:'*'}))
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
